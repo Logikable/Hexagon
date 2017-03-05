350090318da8ecaecc3bccd6c9745f7f0ce57b8d
Logikable <seanluchen@hotmail.com>
2017-03-05 06:07:35 -0500

    geolocator

--- StealthApp/Program.cs
+++ StealthApp/Program.cs
@@ -16,7 +16,7 @@
 		private static String ACCOUNT_SID = "ACa520c27feb8d014ad7c0645989152fc7";
 		private static String AUTH_TOKEN = "acd6380c8687a036873255f3aa66d42e";
 		private static String APP_ID = "AP7adefab30e2165ca3a7764d977658a8c";
-		private static String HARDWARE_URL = "https://1b9793f8.ngrok.io";
+		private static String HARDWARE_URL = "https://922a34fa.ngrok.io";
 		private static String FROM_NUMBER = "+16262262433";
 		private static String TO_NUMBER = "+15106849508";
 		private static String EMERGENCY = "+15106314655";
@@ -97,8 +97,22 @@
 					}
 					else if (match3)
 					{
-						p.StartInfo.Arguments = @" -jar " + DIR + "\\TwilioTest.jar msg " + param;
-						p.Start();
+					//	p.StartInfo.Arguments = @" -jar " + DIR + "\\TwilioTest.jar msg " + param;
+					//	p.Start();
+						ProcessStartInfo pythonInfo = new ProcessStartInfo("python");
+						pythonInfo.UseShellExecute = false;
+						pythonInfo.RedirectStandardOutput = true;
+						pythonInfo.Arguments = DIR + "\\geolocator.py";
+
+						Process py = new Process();
+						py.StartInfo = pythonInfo;
+						py.Start();
+
+						StreamReader sr = py.StandardOutput;
+						Console.WriteLine(sr.ReadLine());
+						py.WaitForExit();
+						py.Close();
+
 						Console.WriteLine("912");
 					}
 
